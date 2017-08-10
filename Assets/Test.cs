42cdce9ca3c83a0c39bcc28a1462a3b5c9c98ba7
Egor Safronov <safronovrm@gmail.com>
2017-08-10 11:59:14 +0300

    GetTransfrom

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class Test : MonoBehaviour
+{
 
+	private Transform _transform;
 	// Use this for initialization
 	void Start () {
+		_transform = GetComponent<Transform>;
 	}
 	
 	// Update is called once per frame
